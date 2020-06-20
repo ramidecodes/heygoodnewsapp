01c8132e01663eb9186d5256b4d998403d8dc082
Ramiro Ramirez <ram@foryouandyourcustomers.com>
2020-06-20 22:03:21 +0200

    Move Google Analytics code to the head

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: 'UA-170137202-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
       },
     },
     'gatsby-plugin-sass',
